cddf240a81776b3e559a1645518c10bd43dfef99
dweller4085 <immbirj@gmail.com>
2023-10-10 00:15:27 +0700

    more page stuff and styling

--- lab5/script.js
+++ lab5/script.js
@@ -1,6 +1,9 @@
 
-function open_popup(e) {
+function openPopup(e) {
     console.log(e);
 }
 
-[...document.getElementsByClassName('news-article')].forEach(e => e.addEventListener('click', open_popup));
+[...document.getElementsByClassName('news-article')].forEach(e => {
+    e.addEventListener('click', openPopup);
+    e.setAttribute('data-full-contents', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.');
+});
